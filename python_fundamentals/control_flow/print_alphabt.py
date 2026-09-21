01d045f2f60a3ab55efb95c0048364b80d8a72eb
Tyren Howarth <14807@holbertonstudents.com>
2026-09-21 15:26:34 +1000

    Fixed sort list

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python3
-print('abcdfghijklmnoprstuvwxyz')
+#forbidden values 101, 113
+for x in range (97, 123):
+    if x == 101 or x == 113:
+        continue
+    print(chr(x), end="")
