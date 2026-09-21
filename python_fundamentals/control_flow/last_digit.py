e5c0d3b87bed8423fd357f0ecd9a6cb948975438
Tyren Howarth <14807@holbertonstudents.com>
2026-09-21 14:03:52 +1000

    Fixed errors, shortend code

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10000, 10000)
-lastnumber = abs(number) % 10
 
-if lastnumber > 5:
-    print('Last digit of ' f'{number} ' 'is ' f'{lastnumber} ' 'and is greater than 5')
-if lastnumber == 0:
-    print('Last digit of ' f'{number} ' 'is ' f'{lastnumber} ' 'and is 0')
-if lastnumber <5 and lastnumber != 0:
-    print('Last digit of ' f'{number} ' 'is ' f'{lastnumber} ' 'and is less than 6 and not 0')
+if number > 0:
+    digit = abs(number) % 10
+else:
+    number < 0
+    digit = -(abs(number) % 10)
+
+if digit > 5:
+    print(f'Last digit of {number} is {digit} and is greater than 5')
+elif digit == 0:
+    print(f'Last digit of {number} is {digit} and is 0')
+else:
+    print(f'Last digit of {number} is {digit} and is less than 6 and not 0')
