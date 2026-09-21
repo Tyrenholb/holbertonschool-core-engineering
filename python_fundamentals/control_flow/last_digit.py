8248dd7bae930ef34760dc7cd331601d9eba343f
Tyren Howarth <14807@holbertonstudents.com>
2026-09-21 13:20:16 +1000

    First attempt task 1

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -3,8 +3,8 @@ number = __import__('random').randint(-10000, 10000)
 lastnumber = abs(number) % 10
 
 if lastnumber > 5:
-    print('last digit of ' f'{number} ' 'is ' f'{lastnumber} ' 'and is greater than 5')
+    print('Last digit of ' f'{number} ' 'is ' f'{lastnumber} ' 'and is greater than 5')
 if lastnumber == 0:
-    print('last digit of ' f'{number} ' 'is ' f'{lastnumber} ' 'and is 0')
+    print('Last digit of ' f'{number} ' 'is ' f'{lastnumber} ' 'and is 0')
 if lastnumber <5 and lastnumber != 0:
-    print('last digit of ' f'{number} ' 'is ' f'{lastnumber} ' 'and is less than 6 and not 0')
+    print('Last digit of ' f'{number} ' 'is ' f'{lastnumber} ' 'and is less than 6 and not 0')
