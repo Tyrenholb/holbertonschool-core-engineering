891704334fa4aee5ce1b64edd5efe84af0be2ca0
Tyren Howarth <14807@holbertonstudents.com>
2026-09-21 11:03:57 +1000

    first attempt, updated respo to match intranet requirements

---
+++ python_fundamentals/hello_world/README.md
@@ -0,0 +1,1 @@
+More intro tasks to python.
---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+pi = 3.14159
+valid = 2 + 2 == 4
+
+print('Language: Python')
+print('Version: 3')
+print(f"Pi approx: {pi:.2f}")
+print(f"Computation valid: {valid}")
