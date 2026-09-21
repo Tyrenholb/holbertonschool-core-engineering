1928babefc4276c3affc9c701c42090fe2950dca
Tyren Howarth <14807@holbertonstudents.com>
2026-09-21 15:31:24 +1000

    Fixed errors

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-#forbidden values 101, 113
-for x in range (97, 123):
+for x in range(97, 123):
     if x == 101 or x == 113:
         continue
-    print(chr(x), end="")
+    print("{}".format(chr(x)), end="")
