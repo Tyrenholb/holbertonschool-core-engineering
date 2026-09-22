9dd8ab6053c8ee75874c5e1834388ae365137fe1
Tyren Howarth <14807@holbertonstudents.com>
2026-09-22 10:55:52 +1000

    First attempt at task 1

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    result = ""
+
+    for chars in str:
+        if 97 <= ord(chars) <= 122:
+            chars = chr(ord(chars) - 32)
+        result += chars
+
+    print(result)
