a8e24cedae9fe3e7aaee56f4d5f4fc2b69a3dcfd
Tyren Howarth <14807@holbertonstudents.com>
2026-09-22 12:03:20 +1000

    second attempt task 3

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 def pow(a, b):
-    return a * b
+    result = 1
+
+    for _ in range(abs(b)):
+        result *= a
+
+    if b < 0:
+        result = 1 / result
+
+    return result
